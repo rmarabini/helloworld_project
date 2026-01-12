dd73d77e27c3a44e58de87b7e0923a2c8159c245
Your Name <you@example.com>
2026-01-12 13:16:44 +0100

    add migrations

--- hello/models.py
+++ hello/models.py
@@ -1,3 +1,10 @@
+# Create your models here.
 from django.db import models
 
-# Create your models here.
+
+class Greeting(models.Model):  # provide a different greeting to each user
+    name = models.CharField(max_length=100)
+    message = models.CharField(max_length=200, default="Hello")
+
+    def __str__(self):
+        return f"{self.message}, {self.name}!"

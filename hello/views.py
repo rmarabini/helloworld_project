8eedd29108f45ff5f14e61e9e7febd85419f6098
Your Name <you@example.com>
2026-01-12 13:14:31 +0100

    add models

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Greeting
 # Create your views here.
 
 
@@ -11,6 +12,16 @@ def hello_name_old(request, name="World"):
     return HttpResponse(f"Hello, {name}!")
 
 
+def hello_name_old2(request, name="World"):
+    return render(request, "hello/greeting.html", {"name": name})   
+
+
 def hello_name(request, name="World"):
-    return render(request, "hello/greeting.html", {"name": name})
-    
+    # Try to get a greeting for this name
+    greeting = Greeting.objects.filter(name=name).first()
+    if greeting:
+        message = f"{greeting.message}, {greeting.name}!"
+    else:
+        message = f"Hello, {name}!"
+
+    return render(request, "hello/greeting.html", {"message": message})
